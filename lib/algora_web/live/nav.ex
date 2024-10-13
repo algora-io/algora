c375fc517f4a6a12a14bfe8fbd1e5998f12730db
zafer <zafer@algora.io>
2024-10-13 21:01:53 +0300

    remove unused nav

--- lib/algora_web/live/nav.ex
+++
@@ -1,9 +0,0 @@
-defmodule AlgoraWeb.Nav do
-  use Phoenix.Component
-
-  def on_mount(:default, _params, _session, socket) do
-    {:cont,
-     socket
-     |> assign(:region, System.get_env("FLY_REGION") || "iad")}
-  end
-end

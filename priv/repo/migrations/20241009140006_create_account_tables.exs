83f9c21c02ddfb67c362e238ffbc6744591bf4da
zafer <zafer@algora.io>
2024-10-19 19:50:55 +0300

    squash migrations

--- priv/repo/migrations/20241009140006_create_account_tables.exs
+++ priv/repo/migrations/20241009140006_create_account_tables.exs
@@ -5,17 +5,49 @@ defmodule Algora.Repo.Migrations.CreateAccountTables do
     execute "CREATE EXTENSION IF NOT EXISTS citext", ""
 
     create table(:users) do
-      add :email, :citext, null: false
+      add :provider, :string
+      add :provider_id, :string
+      add :provider_login, :string
+      add :provider_meta, :map
+
+      add :email, :citext
       add :name, :string
-      add :handle, :citext, null: false
+      add :handle, :citext
       add :avatar_url, :string
       add :external_homepage_url, :string
+
+      add :type, :string, null: false, default: "individual"
+      add :bio, :text
+      add :location, :string
+      add :stargazers_count, :integer, null: false, default: 0
+      add :domain, :string
+      add :tech_stack, {:array, :string}, null: false, default: "{}"
+      add :featured, :boolean, null: false, default: false
+      add :priority, :integer, null: false, default: 0
+      add :fee_pct, :integer, null: false, default: 19
+      add :seeded, :boolean, null: false, default: false
+      add :activated, :boolean, null: false, default: false
+      add :max_open_attempts, :integer, null: false, default: 3
+      add :manual_assignment, :boolean, null: false, default: false
+      add :bounty_mode, :string, null: false, default: "community"
+
+      add :website_url, :string
+      add :twitter_url, :string
+      add :github_url, :string
+      add :youtube_url, :string
+      add :twitch_url, :string
+      add :discord_url, :string
+      add :slack_url, :string
+      add :linkedin_url, :string
 
       timestamps()
     end
 
     create unique_index(:users, [:email])
     create unique_index(:users, [:handle])
+    create unique_index(:users, [:provider, :provider_id])
+    create index(:users, [:featured])
+    create index(:users, [:priority])
 
     create table(:identities) do
       add :user_id, references(:users, on_delete: :delete_all), null: false
--- priv/repo/migrations/20241014142622_make_handle_nullable.exs
+++
@@ -1,9 +0,0 @@
-defmodule Algora.Repo.Migrations.AddFieldsToUsers do
-  use Ecto.Migration
-
-  def change do
-    alter table(:users) do
-      modify :handle, :string, null: true
-    end
-  end
-end

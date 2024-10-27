2fa27b611a8c225d69943fccc90ca21867be787f
zafer <zafer@algora.io>
2024-10-27 22:38:32 +0200

    add salad ui components

---
+++ lib/algora_web/components/skeleton.ex
@@ -0,0 +1,25 @@
+defmodule AlgoraWeb.Component.Skeleton do
+  @moduledoc false
+  use AlgoraWeb.Component
+
+  @doc """
+  Render skeleton
+  """
+  attr :class, :string, default: nil
+  attr :rest, :global
+
+  def skeleton(assigns) do
+    ~H"""
+    <div
+      class={
+        classes([
+          "animate-pulse rounded-md bg-muted",
+          @class
+        ])
+      }
+      {@rest}
+    >
+    </div>
+    """
+  end
+end

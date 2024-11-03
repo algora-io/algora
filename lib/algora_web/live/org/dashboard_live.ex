4dc6046ac47e45f60c6b7d65cff13b98b38daf8e
zafer <zafer@algora.io>
2024-11-03 04:08:01 +0200

    update theming

--- lib/algora_web/live/org/dashboard_live.ex
+++ lib/algora_web/live/org/dashboard_live.ex
@@ -7,7 +7,7 @@ defmodule AlgoraWeb.Org.DashboardLive do
 
   def mount(_params, _session, socket) do
     org_id = socket.assigns.current_org.id
-    recent_bounties = Bounties.list_bounties(owner_id: org_id, limit: 10)
+    recent_bounties = Bounties.list_bounties(limit: 10)
 
     changeset =
       %BountyForm{}
@@ -25,13 +25,13 @@ defmodule AlgoraWeb.Org.DashboardLive do
   def render(assigns) do
     ~H"""
     <div class="flex-1 space-y-4 p-4 pt-6 sm:p-6 md:p-8">
-      <div class="p-6 relative rounded-xl border border-white/10 bg-white/[2%] bg-gradient-to-br from-white/[2%] via-white/[2%] to-white/[2%] md:gap-8 h-full">
+      <div class="p-6 relative rounded-xl border bg-card text-card-foreground md:gap-8 h-full">
         <div class="flex justify-between items-center">
           <h2 class="text-2xl font-semibold mb-6">Create New Bounty</h2>
           <.button
             type="submit"
             phx-disable-with="Creating..."
-            class="bg-white text-gray-900 hover:bg-gray-100"
+            class="bg-primary text-primary-foreground hover:bg-accent"
           >
             <.icon name="tabler-plus" class="w-4 h-4 mr-2" /> Create Bounty
           </.button>
@@ -47,35 +47,38 @@ defmodule AlgoraWeb.Org.DashboardLive do
                 field={@form[:title]}
                 placeholder="Brief description of the task"
                 required
-                class="w-full bg-gray-900/50 border-white/10 rounded-lg"
+                class="w-full bg-background border-input rounded-lg"
               />
             </div>
             <div>
               <.label for="task_url" class="text-sm font-medium mb-2">
                 Ticket
-                <span class="font-normal text-gray-300 text-xs">
+                <span class="font-normal text-muted-foreground  ">
                   (GitHub, Linear, Figma, Jira, Google Docs...)
                 </span>
               </.label>
               <div class="relative">
                 <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                  <.icon name={get_url_icon(@form[:task_url].value)} class="w-5 h-5 text-gray-400" />
+                  <.icon
+                    name={get_url_icon(@form[:task_url].value)}
+                    class="w-5 h-5 text-muted-foreground"
+                  />
                 </div>
                 <.input
                   type="url"
                   field={@form[:task_url]}
                   placeholder="https://github.com/owner/repo/issues/123"
                   required
-                  class="w-full pl-10 bg-gray-900/50 border-white/10 rounded-lg"
+                  class="w-full pl-10 bg-background border-input rounded-lg"
                 />
               </div>
             </div>
           </div>
 
           <fieldset class="mb-8">
-            <legend class="text-sm font-medium text-gray-300 mb-2">Payment Type</legend>
+            <legend class="text-sm font-medium text-foreground mb-2">Payment Type</legend>
             <div class="mt-1 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-4">
-              <label class={"relative flex cursor-pointer rounded-lg border p-4 shadow-sm focus:outline-none #{if @form[:payment_type].value == "fixed", do: 'border-indigo-600 ring-2 ring-indigo-600 bg-gray-800', else: 'border-gray-700 bg-white/[2.5%]'}"}>
+              <label class={"relative flex cursor-pointer rounded-lg border p-4 shadow-sm focus:outline-none #{if @form[:payment_type].value == "fixed", do: 'border-primary ring-2 ring-primary bg-background', else: 'border-input bg-background/75'}"}>
                 <input
                   type="radio"
                   name="bounty_form[payment_type]"
@@ -85,9 +88,9 @@ defmodule AlgoraWeb.Org.DashboardLive do
                 />
                 <span class="flex flex-1 flex-col">
                   <span class="flex items-center mb-1">
-                    <span class="block text-sm font-medium text-gray-200">Fixed Amount</span>
+                    <span class="block text-sm font-medium text-foreground">Fixed Amount</span>
                     <svg
-                      class={"ml-2 h-5 w-5 text-indigo-600 #{if @form[:payment_type].value != "fixed", do: 'invisible'}"}
+                      class={"ml-2 h-5 w-5 text-primary #{if @form[:payment_type].value != "fixed", do: 'invisible'}"}
                       viewBox="0 0 20 20"
                       fill="currentColor"
                     >
@@ -98,7 +101,7 @@ defmodule AlgoraWeb.Org.DashboardLive do
                       />
                     </svg>
                   </span>
-                  <span class="mt-1 text-sm text-gray-400">
+                  <span class="mt-1 text-sm text-muted-foreground">
                     Pay upon full completion or reward milestones
                   </span>
 
@@ -110,17 +113,17 @@ defmodule AlgoraWeb.Org.DashboardLive do
                         step="0.01"
                         placeholder="$1,000"
                         required={@form[:payment_type].value == "fixed"}
-                        class="font-display w-full py-1.5 bg-gray-900/50 border-white/10 rounded-lg text-sm text-green-300 font-medium"
+                        class="font-display w-full py-1.5 bg-background border-input rounded-lg text-sm text-success font-medium"
                       />
                       <div class="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
-                        <span class="text-gray-400 text-sm">USD</span>
+                        <span class="text-muted-foreground text-sm">USD</span>
                       </div>
                     </div>
                   </div>
                 </span>
               </label>
 
-              <label class={"relative flex cursor-pointer rounded-lg border p-4 shadow-sm focus:outline-none #{if @form[:payment_type].value == "hourly", do: 'border-indigo-600 ring-2 ring-indigo-600 bg-gray-800', else: 'border-gray-700 bg-white/[2.5%]'}"}>
+              <label class={"relative flex cursor-pointer rounded-lg border p-4 shadow-sm focus:outline-none #{if @form[:payment_type].value == "hourly", do: 'border-primary ring-2 ring-primary bg-background', else: 'border-input bg-background/75'}"}>
                 <input
                   type="radio"
                   name="bounty_form[payment_type]"
@@ -130,9 +133,9 @@ defmodule AlgoraWeb.Org.DashboardLive do
                 />
                 <span class="flex flex-1 flex-col">
                   <span class="flex items-center mb-1">
-                    <span class="block text-sm font-medium text-gray-200">Hourly Rate</span>
+                    <span class="block text-sm font-medium text-foreground">Hourly Rate</span>
                     <svg
-                      class={"ml-2 h-5 w-5 text-indigo-600 #{if @form[:payment_type].value != "hourly", do: 'invisible'}"}
+                      class={"ml-2 h-5 w-5 text-primary #{if @form[:payment_type].value != "hourly", do: 'invisible'}"}
                       viewBox="0 0 20 20"
                       fill="currentColor"
                     >
@@ -143,7 +146,7 @@ defmodule AlgoraWeb.Org.DashboardLive do
                       />
                     </svg>
                   </span>
-                  <span class="mt-1 text-sm text-gray-400">
+                  <span class="mt-1 text-sm text-muted-foreground">
                     Pay as you go based on hours worked
                   </span>
 
@@ -156,10 +159,10 @@ defmodule AlgoraWeb.Org.DashboardLive do
                           step="0.01"
                           placeholder="$75"
                           required={@form[:payment_type].value == "hourly"}
-                          class="font-display w-full py-1.5 bg-gray-900/50 border-white/10 rounded-lg text-sm text-green-300 font-medium"
+                          class="font-display w-full py-1.5 bg-background border-input rounded-lg text-sm text-success font-medium"
                         />
                         <div class="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
-                          <span class="text-gray-400 text-sm">USD/h</span>
+                          <span class="text-muted-foreground text-sm">USD/h</span>
                         </div>
                       </div>
                       <div class="relative">
@@ -169,10 +172,10 @@ defmodule AlgoraWeb.Org.DashboardLive do
                           step="1"
                           placeholder="10"
                           required={@form[:payment_type].value == "hourly"}
-                          class="font-display w-full py-1.5 bg-gray-900/50 border-white/10 rounded-lg text-sm"
+                          class="font-display w-full py-1.5 bg-background border-input rounded-lg text-sm"
                         />
                         <div class="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
-                          <span class="text-gray-400 text-sm">hours per week</span>
+                          <span class="text-muted-foreground text-sm">hours per week</span>
                         </div>
                       </div>
                     </div>
@@ -190,7 +193,7 @@ defmodule AlgoraWeb.Org.DashboardLive do
 
   def bounties_card(assigns) do
     ~H"""
-    <div class="group/card relative h-full rounded-xl border border-white/10 bg-white/[2%] bg-gradient-to-br from-white/[2%] via-white/[2%] to-white/[2%] md:gap-8 overflow-hidden lg:col-span-4">
+    <div class="group/card relative h-full rounded-xl border bg-card text-card-foreground md:gap-8 overflow-hidden lg:col-span-4">
       <div class="flex justify-between">
         <div class="flex flex-col space-y-1.5 p-6">
           <h3 class="text-2xl font-semibold leading-none tracking-tight">Bounties</h3>
@@ -198,7 +201,7 @@ defmodule AlgoraWeb.Org.DashboardLive do
         </div>
         <div class="p-6">
           <.link
-            class="whitespace-pre text-sm text-gray-400 hover:underline hover:brightness-125"
+            class="whitespace-pre text-sm text-muted-foreground hover:underline hover:brightness-125"
             href={"/org/#{@current_org.handle}/bounties?status=open"}
           >
             View all
@@ -216,7 +219,7 @@ defmodule AlgoraWeb.Org.DashboardLive do
               >
                 <div class="min-w-0 flex-auto">
                   <div class="flex items-center gap-x-3">
-                    <div class="flex-none rounded-full p-1 bg-emerald-400/10 text-emerald-400">
+                    <div class="flex-none rounded-full p-1 bg-success/10 text-success">
                       <div class="h-2 w-2 rounded-full bg-current"></div>
                     </div>
                     <h2 class="line-clamp-2 min-w-0 text-base font-semibold leading-none text-white group-hover:underline">
@@ -236,7 +239,7 @@ defmodule AlgoraWeb.Org.DashboardLive do
                   </div>
                 </div>
                 <div class="pl-6">
-                  <div class="flex-none rounded-xl px-3 py-1 font-mono text-lg font-extrabold ring-1 ring-inset bg-emerald-400/5 text-emerald-400 ring-emerald-400/30">
+                  <div class="flex-none rounded-xl px-3 py-1 font-mono text-lg font-extrabold ring-1 ring-inset bg-success/5 text-success ring-success/30">
                     <%= Money.format!(bounty.amount, bounty.currency) %>
                   </div>
                 </div>

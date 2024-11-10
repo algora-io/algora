18be48b423c11c8d41e5d876cedaff4fa45c7981
zafer <zafer@algora.io>
2024-11-10 22:37:16 +0200

    init /jobs/:id

--- lib/algora_web/router.ex
+++ lib/algora_web/router.ex
@@ -53,6 +53,7 @@ defmodule AlgoraWeb.Router do
       live "/org/:org_handle/projects", Project.IndexLive, :index
       # live "/org/:org_handle/projects/:id", Project.ViewLive
       live "/org/:org_handle/jobs", Org.JobsLive, :index
+      live "/org/:org_handle/jobs/:id", Org.JobLive, :index
       live "/org/:org_handle/analytics", Org.AnalyticsLive, :index
     end
 

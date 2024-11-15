4ba150c6a13cd17bd9d105694c8cc6f926e1717c
zafer <zafer@algora.io>
2024-11-15 18:21:54 +0200

    add dev testimonials

--- lib/algora_web/components/ui/button.ex
+++ lib/algora_web/components/ui/button.ex
@@ -60,7 +60,7 @@ defmodule AlgoraWeb.Components.UI.Button do
     size: %{
       "default" => "h-9 px-4 py-2 text-sm",
       "sm" => "h-8 rounded-md px-3 text-xs",
-      "lg" => "h-10 rounded-md px-8",
+      "lg" => "h-10 rounded-md px-8 text-base",
       "icon" => "h-9 w-9"
     }
   }

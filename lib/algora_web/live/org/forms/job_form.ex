cbaa02391d8a81e1ecd55bc615bf941b2f4df85d
zafer <zafer@algora.io>
2024-11-15 19:57:08 +0200

    add create job form

---
+++ lib/algora_web/live/org/forms/job_form.ex
@@ -0,0 +1,31 @@
+defmodule AlgoraWeb.Org.Forms.JobForm do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  embedded_schema do
+    field :title, :string
+    field :task_url, :string
+    field :work_type, :string, default: "remote"
+    field :min_compensation, :integer
+    field :max_compensation, :integer
+
+    embeds_many :projects, Project do
+      field :title, :string
+      field :url, :string
+      field :amount, :integer
+    end
+  end
+
+  def changeset(form, attrs \\ %{}) do
+    form
+    |> cast(attrs, [:title, :task_url, :work_type, :min_compensation, :max_compensation])
+    |> validate_required([:title, :task_url, :work_type])
+    |> cast_embed(:projects, with: &project_changeset/2)
+  end
+
+  defp project_changeset(schema, attrs) do
+    schema
+    |> cast(attrs, [:title, :url, :amount])
+    |> validate_required([:title, :url, :amount])
+  end
+end

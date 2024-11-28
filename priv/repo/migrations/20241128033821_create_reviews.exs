b1fb331a48f37cf5a4c5bd7bde739be29e8419e8
zafer <zafer@algora.io>
2024-11-28 03:45:01 +0300

    add migration for reviews

---
+++ priv/repo/migrations/20241128033821_create_reviews.exs
@@ -0,0 +1,22 @@
+defmodule Algora.Repo.Migrations.CreateReviews do
+  use Ecto.Migration
+
+  def change do
+    create table(:reviews) do
+      add :rating, :integer, null: false
+      add :content, :string, null: false
+      add :visibility, :string, null: false, default: "public"
+
+      add :contract_id, references(:contracts, on_delete: :restrict), null: false
+      add :reviewer_id, references(:users, on_delete: :restrict), null: false
+      add :reviewee_id, references(:users, on_delete: :restrict), null: false
+
+      timestamps()
+    end
+
+    # Add indexes for foreign keys and common queries
+    create index(:reviews, [:contract_id])
+    create index(:reviews, [:reviewer_id])
+    create index(:reviews, [:reviewee_id])
+  end
+end

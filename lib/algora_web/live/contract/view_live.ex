7989f7d685d536ad8f74a885877bae349c40a3ea
zafer <zafer@algora.io>
2024-12-05 02:24:05 +0300

    fix typo

--- lib/algora_web/live/contract/view_live.ex
+++ lib/algora_web/live/contract/view_live.ex
@@ -112,8 +112,8 @@ defmodule AlgoraWeb.Contract.ViewLive do
                                   Ready to release payment for <%= timesheet.hours_worked %> hours
                                 </div>
                                 <div class="text-sm text-muted-foreground">
-                                  <%= Calendar.strftime(@contract.start_date, "%b %d") %> - <%= Calendar.strftime(
-                                    @contract.end_date,
+                                  <%= Calendar.strftime(contract.start_date, "%b %d") %> - <%= Calendar.strftime(
+                                    contract.end_date,
                                     "%b %d, %Y"
                                   ) %>
                                 </div>

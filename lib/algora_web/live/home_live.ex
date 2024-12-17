6c6a1c86595371d8bd5a08e128d08a49799f22d6
zafer <zafer@algora.io>
2024-12-17 19:52:19 +0300

    always render homepage sidebar

--- lib/algora_web/live/home_live.ex
+++ lib/algora_web/live/home_live.ex
@@ -218,8 +218,8 @@ defmodule AlgoraWeb.HomeLive do
                 </div>
               </div>
               <!-- Featured devs -->
-              <%= if length(@featured_devs) > 0 do %>
-                <div class="mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
+              <div class="mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0 min-h-screen">
+                <%= if length(@featured_devs) > 0 do %>
                   <div class="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
                     <.dev_card dev={List.first(@featured_devs)} />
                   </div>
@@ -235,8 +235,8 @@ defmodule AlgoraWeb.HomeLive do
                       <.dev_card dev={dev} />
                     <% end %>
                   </div>
-                </div>
-              <% end %>
+                <% end %>
+              </div>
             </div>
           </div>
           <!-- New Footer -->

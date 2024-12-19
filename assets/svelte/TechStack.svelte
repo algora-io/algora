be4cb3e34a80552b10a7d94ea9cf3fae82013a89
zafer <zafer@algora.io>
2024-12-19 03:05:30 +0300

    port tech stack input to svelte

---
+++ assets/svelte/TechStack.svelte
@@ -0,0 +1,71 @@
+<script>
+  export let tech = [];
+  export let live;
+
+  let input = '';
+  let techMap = new Map();
+
+  $: {
+    techMap.clear();
+    tech.forEach(tech => {
+      techMap.set(tech.toLowerCase(), tech);
+    });
+  }
+
+  function addTech(e) {
+    if (e.key === 'Enter' || e.key === ',') {
+      e.preventDefault();
+      const tech = input.trim();
+      const techLower = tech.toLowerCase();
+      
+      if (tech && !techMap.has(techLower)) {
+        techMap.set(techLower, tech);
+        updateTechStack();
+      }
+      input = '';
+    }
+  }
+
+  function removeTech(tech) {
+    techMap.delete(tech.toLowerCase());
+    updateTechStack();
+    
+  }
+
+  function updateTechStack() {
+    const newTechStack = Array.from(techMap.values());
+    tech = newTechStack;
+    live.pushEvent('tech_stack_changed', { tech_stack: tech });
+  }
+</script>
+
+<div class="mt-4">
+  <input
+    type="text"
+    bind:value={input}
+    on:keydown={addTech}
+    placeholder="Elixir, Phoenix, PostgreSQL, etc."
+    class="bg-background block w-full rounded-lg border-input py-[7px] px-[11px] text-foreground focus:outline-none focus:ring-4 sm:text-sm sm:leading-6 phx-no-feedback:border-input phx-no-feedback:focus:border-ring phx-no-feedback:focus:ring-ring/5 focus:border-ring focus:ring-ring/5"
+  />
+
+  <input
+    type="hidden"
+    name="tech_stack_form[tech_stack]"
+    value={JSON.stringify(tech)}
+  />
+
+  <div class="flex flex-wrap gap-3 mt-4">
+    {#each tech as tech}
+      <div class="bg-success/10 text-success rounded-lg px-3 py-1.5 text-sm font-semibold flex items-center">
+        {tech}
+        <button
+          type="button"
+          class="ml-2 text-success hover:text-success/80"
+          on:click={() => removeTech(tech)}
+        >
+          ×
+        </button>
+      </div>
+    {/each}
+  </div>
+</div> 

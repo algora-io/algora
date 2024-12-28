af458e1cc970fc3dfb3216e123bcbcd532c2ee4f
zafer <zafer@algora.io>
2024-12-28 05:46:41 +0300

    add new dashboard

--- lib/algora_web/router.ex
+++ lib/algora_web/router.ex
@@ -42,6 +42,12 @@ defmodule AlgoraWeb.Router do
       live "/admin/analytics", Admin.CompanyAnalyticsLive
     end
 
+    live_session :community,
+      layout: {AlgoraWeb.Layouts, :user},
+      on_mount: [{AlgoraWeb.UserAuth, :ensure_authenticated}, AlgoraWeb.User.Nav] do
+      live "/home", Community.DashboardLive, :index
+    end
+
     live_session :authenticated,
       layout: {AlgoraWeb.Layouts, :user},
       on_mount: [{AlgoraWeb.UserAuth, :ensure_authenticated}, AlgoraWeb.User.Nav] do

d083e590067acca21842706ddf3a24bd7ed0b4c3
zafer <zafer@algora.io>
2024-12-28 05:08:10 +0300

    update column order

--- lib/algora_web/components/bounties.ex
+++ lib/algora_web/components/bounties.ex
@@ -9,16 +9,21 @@ defmodule AlgoraWeb.Components.Bounties do
       <table class="min-w-full divide-y divide-border">
         <thead>
           <tr>
-            <th class="px-6 pl-2 py-2 text-left text-sm font-medium max-w-[300px]">Title</th>
+            <th class="px-6 pl-2 py-2 text-left text-sm font-medium">Amount</th>
+            <th class="px-6 py-2 text-left text-sm font-medium max-w-[300px]">Title</th>
             <th class="px-6 py-2 text-left text-sm font-medium">Issue</th>
-            <th class="px-6 py-2 text-center text-sm font-medium">Sponsors</th>
-            <th class="px-6 pr-2 py-2 text-right text-sm font-medium">Amount</th>
+            <th class="px-6 pr-2 py-2 text-right text-sm font-medium">Sponsors</th>
           </tr>
         </thead>
         <tbody class="divide-y divide-border">
           <%= for ticket <- @tickets do %>
             <tr class="hover:bg-muted/50">
-              <td class="px-6 pl-2 py-2">
+              <td class="px-6 pl-2 py-2 text-left">
+                <div class="font-display text-xl font-semibold text-success whitespace-nowrap tabular-nums">
+                  {Money.to_string!(ticket.total_bounty_amount)}
+                </div>
+              </td>
+              <td class="px-6 py-2">
                 <.link href={ticket.url} class="block text-sm text-foreground hover:underline">
                   {ticket.title}
                 </.link>
@@ -38,8 +43,8 @@ defmodule AlgoraWeb.Components.Bounties do
                   </.link>
                 </div>
               </td>
-              <td class="px-6 py-2">
-                <div class="flex -space-x-2 justify-center">
+              <td class="px-6 pr-2 py-2">
+                <div class="flex -space-x-2 justify-end">
                   <%= for bounty <- Enum.take(ticket.top_bounties, 3) do %>
                     <.link href={"https://github.com/#{bounty.owner.provider_login}"}>
                       <.avatar class="h-8 w-8">
@@ -55,11 +60,6 @@ defmodule AlgoraWeb.Components.Bounties do
                       +{ticket.bounty_count - 3}
                     </div>
                   <% end %>
-                </div>
-              </td>
-              <td class="px-6 pr-2 py-2 text-right">
-                <div class="font-display text-xl font-semibold text-success whitespace-nowrap tabular-nums">
-                  {Money.to_string!(ticket.total_bounty_amount)}
                 </div>
               </td>
             </tr>

d2ae19b3534eb7e2ff1036b42f54bb35552e5c16
zafer <zafer@algora.io>
2024-12-28 19:48:44 +0300

    feat: /tip command response

--- lib/algora/github/poller/comment_consumer.ex
+++ lib/algora/github/poller/comment_consumer.ex
@@ -18,25 +18,45 @@ defmodule Algora.Github.Poller.CommentConsumer do
     command = Util.base64_to_term!(encoded_command)
     ticket_ref = Util.base64_to_term!(encoded_ticket_ref)
 
-    dbg(command)
-    dbg(ticket_ref)
-
     run_command(command, ticket_ref, comment)
+  end
+
+  defp run_command({:tip, args}, ticket_ref, comment) do
+    amount = Keyword.get(args, :amount)
+    recipient = Keyword.get(args, :username)
+    owner = Keyword.get(ticket_ref, :owner)
+    repo = Keyword.get(ticket_ref, :repo)
+    number = Keyword.get(ticket_ref, :number)
+
+    query = URI.encode_query(amount: Money.to_decimal(amount), recipient: recipient)
+    url = AlgoraWeb.Endpoint.url() <> "/tip" <> "?" <> query
+    body = "Please visit [Algora](#{url}) to complete your tip via Stripe."
+
+    if Github.pat_enabled() do
+      Github.create_issue_comment(Github.pat(), owner, repo, number, body)
+    else
+      Logger.info("""
+      Github.create_issue_comment(Github.pat(), "#{owner}", "#{repo}", #{number},
+             \"\"\"
+             #{body}
+             \"\"\")
+      """)
+
+      :ok
+    end
   end
 
   defp run_command({:bounty, args}, ticket_ref, comment) do
     # TODO: use user's own token if available
     token = Github.TokenPool.get_token()
 
+    owner = Keyword.get(ticket_ref, :owner)
+    repo = Keyword.get(ticket_ref, :repo)
+    number = Keyword.get(ticket_ref, :number)
+
     with {:ok, amount} <- Keyword.fetch(args, :amount),
          {:ok, user} <- Workspace.ensure_user(token, extract_actor(comment)),
-         {:ok, ticket} <-
-           Workspace.ensure_ticket(
-             token,
-             Keyword.get(ticket_ref, :owner),
-             Keyword.get(ticket_ref, :repo),
-             Keyword.get(ticket_ref, :number)
-           ),
+         {:ok, ticket} <- Workspace.ensure_ticket(token, owner, repo, number),
          {:ok, _bounty} <-
            Bounties.create_bounty(%{
              creator: user,

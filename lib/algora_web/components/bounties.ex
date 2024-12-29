d0e6edd5ae814ce84a3b05c18c39e8e0cb5d21e4
zafer <zafer@algora.io>
2024-12-29 20:33:43 +0300

    improve bounty board

--- lib/algora_web/components/bounties.ex
+++ lib/algora_web/components/bounties.ex
@@ -9,41 +9,36 @@ defmodule AlgoraWeb.Components.Bounties do
       <table class="min-w-full divide-y divide-border">
         <thead>
           <tr>
-            <th class="px-6 pl-2 py-2 text-left text-sm font-medium">Amount</th>
-            <th class="px-6 py-2 text-left text-sm font-medium max-w-[300px]">Title</th>
-            <th class="px-6 py-2 text-left text-sm font-medium">Issue</th>
-            <th class="px-6 pr-2 py-2 text-right text-sm font-medium">Sponsors</th>
+            <th class="px-2 sm:px-6 pl-2 py-2 text-left text-sm font-medium">Amount</th>
+            <th class="px-2 sm:px-6 py-2 text-left text-sm font-medium max-w-[300px]">Title</th>
+            <th class="px-2 sm:px-6 py-2 text-left text-sm font-medium">Issue</th>
+            <th class="px-2 sm:px-6 pr-2 py-2 text-right text-sm font-medium">Sponsors</th>
           </tr>
         </thead>
         <tbody class="divide-y divide-border">
           <%= for ticket <- @tickets do %>
             <tr class="hover:bg-muted/50">
-              <td class="px-6 pl-2 py-2 text-left">
-                <div class="font-display text-xl font-semibold text-success whitespace-nowrap tabular-nums">
+              <td class="px-2 sm:px-6 pl-2 py-2 text-left">
+                <div class="font-display text-sm sm:text-xl font-semibold text-success whitespace-nowrap tabular-nums">
                   {Money.to_string!(ticket.total_bounty_amount)}
                 </div>
               </td>
-              <td class="px-6 py-2">
-                <.link href={ticket.url} class="block text-sm text-foreground hover:underline">
-                  {ticket.title}
+              <td class="px-2 sm:px-6 py-2">
+                <.link
+                  href={ticket.url}
+                  class="block text-sm text-foreground hover:underline min-w-40"
+                >
+                  <span class="line-clamp-2">{ticket.title}</span>
                 </.link>
               </td>
-              <td class="px-6 py-2">
+              <td class="px-2 sm:px-6 py-2">
                 <div class="flex items-center gap-1 text-sm text-muted-foreground whitespace-nowrap">
-                  <.link
-                    :if={ticket.repository.owner.login}
-                    href={"https://github.com/#{ticket.repository.owner.login}"}
-                    class="font-semibold hover:underline"
-                  >
-                    {ticket.repository.owner.login}
-                  </.link>
-                  <.icon name="tabler-chevron-right" class="h-4 w-4" />
                   <.link href={ticket.url} class="hover:underline">
-                    {ticket.repository.name}#{ticket.number}
+                    <span class="font-semibold mr-1">{ticket.repository.name}</span>#{ticket.number}
                   </.link>
                 </div>
               </td>
-              <td class="px-6 pr-2 py-2">
+              <td class="px-2 sm:px-6 pr-2 py-2">
                 <div class="flex -space-x-2 justify-end">
                   <%= for bounty <- Enum.take(ticket.top_bounties, 3) do %>
                     <.link href={"https://github.com/#{bounty.owner.provider_login}"}>

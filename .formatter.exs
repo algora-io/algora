ddb75b849e0713f1dd5a96cedea0e4a8b40bb888
zafer <zafer@algora.io>
2025-01-03 06:47:53 +0300

    remove tailwind_formatter for now

--- .formatter.exs
+++ .formatter.exs
@@ -1,6 +1,6 @@
 [
   import_deps: [:ecto, :ecto_sql, :phoenix],
   subdirectories: ["priv/*/migrations", "config"],
-  plugins: [TailwindFormatter, Phoenix.LiveView.HTMLFormatter, Styler],
+  plugins: [Phoenix.LiveView.HTMLFormatter, Styler],
   inputs: ["*.{heex,ex,exs}", "{lib,test}/**/*.{heex,ex,exs}", "priv/*/seeds.exs"]
 ]

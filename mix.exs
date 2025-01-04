2c6048c00fb46c62730d9c4ad18c0bdb0062a76c
zafer <zafer@algora.io>
2025-01-05 01:33:41 +0300

    fix: failing checks

--- mix.exs
+++ mix.exs
@@ -75,7 +75,7 @@ defmodule Algora.MixProject do
       {:oban, "~> 2.17"},
       {:styler, "~> 1.2", only: [:dev, :test], runtime: false},
       {:typed_ecto_schema, "~> 0.4.1", runtime: false},
-      {:chameleon, "~> 2.2.0", only: [:dev]}
+      {:chameleon, "~> 2.2.0"}
     ]
   end
 

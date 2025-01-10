ad91c1209e76f27a97227310250aaf65987d6d89
zafer <zafer@algora.io>
2025-01-10 19:13:07 +0300

    restore org dashboards

--- lib/algora_web/controllers/user_auth.ex
+++ lib/algora_web/controllers/user_auth.ex
@@ -185,15 +185,12 @@ defmodule AlgoraWeb.UserAuth do
 
   defp maybe_store_return_to(conn), do: conn
 
-  def signed_in_path(_conn) do
-    # TODO: dynamically determine the path based on the user's context
-    ~p"/home"
-
-    # case get_session(conn, :last_context) do
-    #   nil -> ~p"/dashboard"
-    #   "personal" -> ~p"/dashboard"
-    #   org_handle -> ~p"/org/#{org_handle}"
-    # end
+  def signed_in_path(conn) do
+    case get_session(conn, :last_context) do
+      nil -> ~p"/home"
+      "personal" -> ~p"/home"
+      org_handle -> ~p"/org/#{org_handle}"
+    end
   end
 
   defp login_code_ttl, do: 3600

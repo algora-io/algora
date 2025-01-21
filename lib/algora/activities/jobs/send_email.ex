5eb7d4d5d261302ae95f33ae19a518fe71d2ac0f
ty <lastcanal@gmail.com>
2025-01-20 23:02:53 -0500

    send email job

--- lib/algora/activities/jobs/send_email.ex
+++ lib/algora/activities/jobs/send_email.ex
@@ -5,6 +5,11 @@ defmodule Algora.Activities.SendEmail do
     max_attempts: 1,
     tags: ["email", "activities"]
 
+  alias Swoosh.Email
+
+  @from_name "Algora"
+  @from_email "info@algora.io"
+
   # unique: [period: 30]
 
   def changeset(attrs) do
@@ -12,8 +17,20 @@ defmodule Algora.Activities.SendEmail do
   end
 
   @impl Oban.Worker
-  def perform(%Oban.Job{args: %{}} = job) do
-    IO.inspect(job)
-    :ok
+  def perform(%Oban.Job{args: args}) do
+    case args do
+      %{"email" => email, "name" => name, "title" => subject, "body" => body} ->
+        email =
+          Email.new()
+          |> Email.to({name, email})
+          |> Email.from({@from_name, @from_email})
+          |> Email.subject(subject)
+          |> Email.text_body(body)
+
+        Algora.Mailer.deliver(email)
+
+      _args ->
+        :discard
+    end
   end
 end

962bcd1ea9c6104f770bed499267e1bff82fda7c
zafer <zafer@algora.io>
2025-02-03 17:40:40 +0300

    fix overflows

--- lib/algora_web/live/home_live.ex
+++ lib/algora_web/live/home_live.ex
@@ -129,7 +129,7 @@ defmodule AlgoraWeb.HomeLive do
       </header>
 
       <main>
-        <div class="relative isolate">
+        <div class="relative isolate overflow-hidden">
           <!-- Background pattern -->
           <div
             class="absolute inset-x-0 -top-40 -z-10 transform overflow-hidden blur-3xl sm:-top-80"
@@ -164,10 +164,7 @@ defmodule AlgoraWeb.HomeLive do
             />
           </div>
 
-          <div
-            class="top-[calc(100%-13rem)] absolute inset-x-0 -z-10 transform overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-            aria-hidden="true"
-          >
+          <div class="absolute inset-x-0 -z-10 transform overflow-hidden blur-3xl" aria-hidden="true">
             <div
               class="left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] relative -translate-x-1/2 bg-gradient-to-tr from-primary to-secondary opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
               style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"

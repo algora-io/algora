7311d46840ce6855e88efd17c69b63ac0a9f4f94
zafer <zafer@algora.io>
2025-02-04 19:47:57 +0300

    hide contacts on sidebar for now

--- lib/algora_web/live/org/nav.ex
+++ lib/algora_web/live/org/nav.ex
@@ -8,29 +8,32 @@ defmodule AlgoraWeb.Org.Nav do
 
   def on_mount(:default, %{"org_handle" => org_handle}, _session, socket) do
     current_org = Organizations.get_org_by(handle: org_handle)
-    members = Organizations.list_org_members(current_org)
-    contractors = Organizations.list_org_contractors(current_org)
 
-    online_users =
-      (contractors ++ members)
-      |> Enum.uniq_by(& &1.id)
-      |> Enum.reject(&(&1.id == socket.assigns.current_user.id))
-      |> Enum.map(fn user ->
-        %{
-          id: user.id,
-          name: user.name || user.handle,
-          handle: user.handle,
-          avatar_url: user.avatar_url,
-          type: :individual
-        }
-      end)
+    # TODO: restore once chat is implemented
+    contacts = []
+
+    # members = Organizations.list_org_members(current_org)
+    # contractors = Organizations.list_org_contractors(current_org)
+    # contacts =
+    #   (contractors ++ members)
+    #   |> Enum.uniq_by(& &1.id)
+    #   |> Enum.reject(&(&1.id == socket.assigns.current_user.id))
+    #   |> Enum.map(fn user ->
+    #     %{
+    #       id: user.id,
+    #       name: user.name || user.handle,
+    #       handle: user.handle,
+    #       avatar_url: user.avatar_url,
+    #       type: :individual
+    #     }
+    #   end)
 
     {:cont,
      socket
      |> assign(:new_bounty_form, to_form(%{"github_issue_url" => "", "amount" => ""}))
      |> assign(:current_org, current_org)
      |> assign(:nav, nav_items(current_org.handle))
-     |> assign(:online_users, online_users)
+     |> assign(:contacts, contacts)
      |> attach_hook(:active_tab, :handle_params, &handle_active_tab_params/3)}
   end
 

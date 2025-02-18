2b4dc5c846bebc8342eb90c96efbeae53829cc8d
zafer <zafer@algora.io>
2025-02-18 22:31:34 +0300

    fix 3rd party behavior callbacks

--- lib/algora/integrations/github/behaviour.ex
+++ lib/algora/integrations/github/behaviour.ex
@@ -2,24 +2,25 @@ defmodule Algora.Github.Behaviour do
   @moduledoc false
 
   @type token :: String.t()
-  @type response :: {:ok, map()} | {:error, any()}
 
-  @callback get_issue(token(), String.t(), String.t(), integer()) :: response
-  @callback get_repository(token(), String.t(), String.t()) :: response
-  @callback get_repository(token(), integer()) :: response
-  @callback get_pull_request(token(), String.t(), String.t(), integer()) :: response
-  @callback get_current_user(token()) :: response
-  @callback get_current_user_emails(token()) :: response
-  @callback get_user(token(), integer()) :: response
-  @callback get_user_by_username(token(), String.t()) :: response
-  @callback get_repository_permissions(token(), String.t(), String.t(), String.t()) :: response
-  @callback list_installations(token(), integer()) :: response
-  @callback find_installation(token(), integer(), integer()) :: response
-  @callback get_installation_token(integer()) :: response
-  @callback create_issue_comment(token(), String.t(), String.t(), integer(), String.t()) :: response
-  @callback update_issue_comment(token(), String.t(), String.t(), integer(), String.t()) :: response
-  @callback list_repository_events(token(), String.t(), String.t(), keyword()) :: response
-  @callback list_repository_comments(token(), String.t(), String.t(), keyword()) :: response
-  @callback add_labels(token(), String.t(), String.t(), integer(), [String.t()]) :: response
-  @callback render_markdown(token(), String.t(), keyword()) :: response
+  @callback get_issue(token(), String.t(), String.t(), integer()) :: {:ok, map()} | {:error, String.t()}
+  @callback get_repository(token(), String.t(), String.t()) :: {:ok, map()} | {:error, String.t()}
+  @callback get_repository(token(), integer()) :: {:ok, map()} | {:error, String.t()}
+  @callback get_pull_request(token(), String.t(), String.t(), integer()) :: {:ok, map()} | {:error, String.t()}
+  @callback get_current_user(token()) :: {:ok, map()} | {:error, String.t()}
+  @callback get_current_user_emails(token()) :: {:ok, [map()]} | {:error, String.t()}
+  @callback get_user(token(), integer()) :: {:ok, map()} | {:error, String.t()}
+  @callback get_user_by_username(token(), String.t()) :: {:ok, map()} | {:error, String.t()}
+  @callback get_repository_permissions(token(), String.t(), String.t(), String.t()) :: {:ok, map()} | {:error, String.t()}
+  @callback list_installations(token(), integer()) :: {:ok, map()} | {:error, String.t()}
+  @callback find_installation(token(), integer(), integer()) :: {:ok, map()} | {:error, String.t()}
+  @callback get_installation_token(integer()) :: {:ok, map()} | {:error, String.t()}
+  @callback create_issue_comment(token(), String.t(), String.t(), integer(), String.t()) ::
+              {:ok, map()} | {:error, String.t()}
+  @callback update_issue_comment(token(), String.t(), String.t(), integer(), String.t()) ::
+              {:ok, map()} | {:error, String.t()}
+  @callback list_repository_events(token(), String.t(), String.t(), keyword()) :: {:ok, [map()]} | {:error, String.t()}
+  @callback list_repository_comments(token(), String.t(), String.t(), keyword()) :: {:ok, [map()]} | {:error, String.t()}
+  @callback add_labels(token(), String.t(), String.t(), integer(), [String.t()]) :: {:ok, [map()]} | {:error, String.t()}
+  @callback render_markdown(token(), String.t(), keyword()) :: {:ok, map()} | {:error, String.t()}
 end

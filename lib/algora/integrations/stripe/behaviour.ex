94ae37c066b02fc87345d22c8edf6c7e655fa273
zafer <zafer@algora.io>
2025-02-18 22:31:34 +0300

    fix 3rd party behavior callbacks

--- lib/algora/integrations/stripe/behaviour.ex
+++ lib/algora/integrations/stripe/behaviour.ex
@@ -1,8 +1,8 @@
 defmodule Algora.Stripe.Behaviour do
   @moduledoc false
-  @callback create_invoice(map()) :: {:ok, map()} | {:error, any()}
-  @callback create_invoice_item(map()) :: {:ok, map()} | {:error, any()}
-  @callback pay_invoice(String.t(), map()) :: {:ok, map()} | {:error, any()}
-  @callback create_transfer(map()) :: {:ok, map()} | {:error, any()}
-  @callback create_session(map()) :: {:ok, map()} | {:error, any()}
+  @callback create_invoice(map()) :: {:ok, Stripe.Invoice.t()} | {:error, Stripe.Error.t()}
+  @callback create_invoice_item(map()) :: {:ok, Stripe.Invoiceitem.t()} | {:error, Stripe.Error.t()}
+  @callback pay_invoice(Stripe.id() | Stripe.Invoice.t(), map()) :: {:ok, Stripe.Invoice.t()} | {:error, Stripe.Error.t()}
+  @callback create_transfer(map()) :: {:ok, Stripe.Transfer.t()} | {:error, Stripe.Error.t()}
+  @callback create_session(map()) :: {:ok, Stripe.Session.t()} | {:error, Stripe.Error.t()}
 end

0bcf004547269bb51982b76a7fa472cdedc17f73
zafer <zafer@algora.io>
2025-02-20 23:50:13 +0300

    allow overriding stripe client in runtime

--- lib/algora/integrations/stripe/stripe.ex
+++ lib/algora/integrations/stripe/stripe.ex
@@ -1,43 +1,35 @@
 defmodule Algora.Stripe do
   @moduledoc false
 
+  def client(module) do
+    :algora
+    |> Application.get_env(:stripe_client, Stripe)
+    |> Module.concat(module |> Module.split() |> List.last())
+  end
+
   defmodule Invoice do
     @moduledoc false
-    @stripe Application.compile_env(:algora, :stripe_client, Stripe)
 
-    def create(params) do
-      @stripe.Invoice.create(params)
-    end
+    def create(params), do: Algora.Stripe.client(__MODULE__).create(params)
 
-    def pay(invoice_id, params) do
-      @stripe.Invoice.pay(invoice_id, params)
-    end
+    def pay(invoice_id, params), do: Algora.Stripe.client(__MODULE__).pay(invoice_id, params)
   end
 
   defmodule Invoiceitem do
     @moduledoc false
-    @stripe Application.compile_env(:algora, :stripe_client, Stripe)
 
-    def create(params) do
-      @stripe.Invoiceitem.create(params)
-    end
+    def create(params), do: Algora.Stripe.client(__MODULE__).create(params)
   end
 
   defmodule Transfer do
     @moduledoc false
-    @stripe Application.compile_env(:algora, :stripe_client, Stripe)
 
-    def create(params) do
-      @stripe.Transfer.create(params)
-    end
+    def create(params), do: Algora.Stripe.client(__MODULE__).create(params)
   end
 
   defmodule Session do
     @moduledoc false
-    @stripe Application.compile_env(:algora, :stripe_client, Stripe)
 
-    def create(params) do
-      @stripe.Session.create(params)
-    end
+    def create(params), do: Algora.Stripe.client(__MODULE__).create(params)
   end
 end

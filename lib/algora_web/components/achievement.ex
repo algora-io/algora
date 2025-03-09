21f7f4fdf02e7a10768c9d29c22363c5d2917b0f
zafer <zafer@algora.io>
2025-03-09 13:50:17 +0200

    feat: add hover states to achievement components

--- lib/algora_web/components/achievement.ex
+++ lib/algora_web/components/achievement.ex
@@ -20,11 +20,27 @@ defmodule AlgoraWeb.Components.Achievement do
   def achievement(%{achievement: %{status: :upcoming, path: path}} = assigns) do
     ~H"""
     <.maybe_link navigate={path}>
-      <div class="group flex items-center gap-3">
+      <div class={
+        classes([
+          "group flex items-center gap-3",
+          path && "hover:text-foreground"
+        ])
+      }>
         <div class="flex h-5 w-5 items-center justify-center">
-          <div class="h-2 w-2 rounded-full bg-muted-foreground"></div>
+          <div class={
+            classes([
+              "h-2 w-2 rounded-full bg-muted-foreground",
+              path && "group-hover:bg-foreground"
+            ])
+          }>
+          </div>
         </div>
-        <span class="text-sm font-medium text-muted-foreground">
+        <span class={
+          classes([
+            "text-sm font-medium text-muted-foreground",
+            path && "group-hover:text-foreground"
+          ])
+        }>
           {@achievement.name}
         </span>
       </div>
@@ -35,15 +51,38 @@ defmodule AlgoraWeb.Components.Achievement do
   def achievement(%{achievement: %{status: :current, path: path}} = assigns) do
     ~H"""
     <.maybe_link navigate={path}>
-      <div class="flex items-start" aria-current="step">
+      <div
+        class={[
+          "group flex items-start",
+          path && "hover:text-foreground"
+        ]}
+        aria-current="step"
+      >
         <span
           class="relative flex h-5 w-5 flex-shrink-0 items-center justify-center"
           aria-hidden="true"
         >
-          <span class="absolute h-5 w-5 animate-pulse rounded-full bg-success/25"></span>
-          <span class="relative block h-2 w-2 rounded-full bg-success"></span>
+          <span class={
+            classes([
+              "absolute h-5 w-5 animate-pulse rounded-full bg-success/25",
+              path && "group-hover:bg-foreground/25"
+            ])
+          }>
+          </span>
+          <span class={
+            classes([
+              "relative block h-2 w-2 rounded-full bg-success",
+              path && "group-hover:bg-foreground"
+            ])
+          }>
+          </span>
         </span>
-        <span class="ml-3 text-sm font-medium text-muted-foreground">
+        <span class={
+          classes([
+            "ml-3 text-sm font-medium text-muted-foreground",
+            path && "group-hover:text-foreground"
+          ])
+        }>
           {@achievement.name}
         </span>
       </div>

e9c2e90f69f123f8dec04824ab4f5f8e44dcc39a
zafer <zafer@algora.io>
2025-03-09 13:46:30 +0200

    feat: add links to upcoming user dashboard achievements

--- lib/algora_web/components/achievement.ex
+++ lib/algora_web/components/achievement.ex
@@ -17,30 +17,37 @@ defmodule AlgoraWeb.Components.Achievement do
     """
   end
 
-  def achievement(%{achievement: %{status: :upcoming}} = assigns) do
+  def achievement(%{achievement: %{status: :upcoming, path: path}} = assigns) do
     ~H"""
-    <div class="group flex items-center gap-3">
-      <div class="flex h-5 w-5 items-center justify-center">
-        <div class="h-2 w-2 rounded-full bg-muted-foreground"></div>
+    <.maybe_link navigate={path}>
+      <div class="group flex items-center gap-3">
+        <div class="flex h-5 w-5 items-center justify-center">
+          <div class="h-2 w-2 rounded-full bg-muted-foreground"></div>
+        </div>
+        <span class="text-sm font-medium text-muted-foreground">
+          {@achievement.name}
+        </span>
       </div>
-      <span class="text-sm font-medium text-muted-foreground">
-        {@achievement.name}
-      </span>
-    </div>
+    </.maybe_link>
     """
   end
 
-  def achievement(%{achievement: %{status: :current}} = assigns) do
+  def achievement(%{achievement: %{status: :current, path: path}} = assigns) do
     ~H"""
-    <div class="flex items-start" aria-current="step">
-      <span class="relative flex h-5 w-5 flex-shrink-0 items-center justify-center" aria-hidden="true">
-        <span class="absolute h-5 w-5 animate-pulse rounded-full bg-success/25"></span>
-        <span class="relative block h-2 w-2 rounded-full bg-success"></span>
-      </span>
-      <span class="ml-3 text-sm font-medium text-muted-foreground">
-        {@achievement.name}
-      </span>
-    </div>
+    <.maybe_link navigate={path}>
+      <div class="flex items-start" aria-current="step">
+        <span
+          class="relative flex h-5 w-5 flex-shrink-0 items-center justify-center"
+          aria-hidden="true"
+        >
+          <span class="absolute h-5 w-5 animate-pulse rounded-full bg-success/25"></span>
+          <span class="relative block h-2 w-2 rounded-full bg-success"></span>
+        </span>
+        <span class="ml-3 text-sm font-medium text-muted-foreground">
+          {@achievement.name}
+        </span>
+      </div>
+    </.maybe_link>
     """
   end
 end

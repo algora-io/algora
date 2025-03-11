4e15a3ee9f6e607cd1b9cd8b7f4d9b6722220eec
Zafer Cesur <zafer@algora.io>
2025-03-11 14:23:30 +0200

    delete obsolete code (#88)

---
+++ priv/repo/migrations/20250311110634_drop_activities_tables.exs
@@ -0,0 +1,9 @@
+defmodule Algora.Repo.Migrations.DropActivitiesTables do
+  use Ecto.Migration
+
+  def change do
+    drop table(:job_activities)
+    drop table(:application_activities)
+    drop table(:project_activities)
+  end
+end

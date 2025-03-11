c4bc144d2492731708f6dbac14af0ec9297262a1
zafer <zafer@algora.io>
2025-03-11 14:19:56 +0200

    delete more stuff

--- lib/algora_web/router.ex
+++ lib/algora_web/router.ex
@@ -66,7 +66,6 @@ defmodule AlgoraWeb.Router do
       on_mount: [{AlgoraWeb.UserAuth, :current_user}, AlgoraWeb.Org.Nav] do
       live "/org/:org_handle", Org.DashboardLive, :index
       live "/org/:org_handle/home", Org.DashboardPublicLive, :index
-      live "/org/:org_handle/bounties/new", Org.CreateBountyLive, :new
       live "/org/:org_handle/bounties", Org.BountiesLive, :index
       live "/org/:org_handle/contracts/:id", Contract.ViewLive
       live "/org/:org_handle/team", Org.TeamLive, :index

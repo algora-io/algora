a1d81a1b03bd92f2bd9489c02518f8c0ecf63925
Zafer Cesur <zafer@algora.io>
2025-03-19 15:15:36 +0200

    feat: homepage (#95)

--- lib/algora_web/constants.ex
+++ lib/algora_web/constants.ex
@@ -11,6 +11,7 @@ defmodule AlgoraWeb.Constants do
       github_url: "https://github.com/algora-io",
       # TODO: update this to the new repo
       github_repo_url: "https://github.com/algora-io/tv",
+      github_repo_api_url: "https://api.github.com/repos/algora-io/tv",
       docs_url: "https://docs.algora.io",
       docs_supported_countries_url: "https://docs.algora.io/bounties/payments#supported-countries-regions",
       demo_url: "https://www.youtube.com/watch?v=Ts5GhlEROrs",

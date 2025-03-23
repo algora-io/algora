3f097dfbe3d5da64781eda9ff250339e4040215a
zafer <zafer@algora.io>
2025-03-23 20:24:05 +0200

    chore: move assets_url and ingest_url configuration to prod.exs from runtime.exs

--- config/prod.exs
+++ config/prod.exs
@@ -27,5 +27,8 @@ config :algora,
   dev_routes: true,
   require_admin_for_mailbox: true
 
+config :algora, :assets_url, System.get_env("ASSETS_URL")
+config :algora, :ingest_url, System.get_env("INGEST_URL")
+
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.

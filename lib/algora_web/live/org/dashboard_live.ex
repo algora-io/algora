5ec84aaef2b1ebfef09f9ac7937f55f583b3da1b
zafer <zafer@algora.io>
2025-03-23 22:20:32 +0200

    refactor: conditionally render share badge and image in dashboard layout for better organization

--- lib/algora_web/live/org/dashboard_live.ex
+++ lib/algora_web/live/org/dashboard_live.ex
@@ -1246,41 +1246,43 @@ defmodule AlgoraWeb.Org.DashboardLive do
           <% end %>
         </ol>
       </nav>
-      <div class="pt-12 flex items-center justify-between">
-        <h2 class="text-xl font-semibold leading-none tracking-tight">Share your bounty board</h2>
-      </div>
-      <.badge
-        id="og-url"
-        phx-hook="CopyToClipboard"
-        data-value={url(~p"/org/#{@current_org.handle}/home")}
-        data-notice="Copied to clipboard!"
-        phx-click={
-          %JS{}
-          |> JS.hide(
-            to: "#og-url-copy-icon",
-            transition: {"transition-opacity", "opacity-100", "opacity-0"}
-          )
-          |> JS.show(
-            to: "#og-url-check-icon",
-            transition: {"transition-opacity", "opacity-0", "opacity-100"}
-          )
-        }
-        class="relative cursor-pointer mt-3 text-foreground/90 hover:text-foreground"
-        variant="outline"
-      >
-        <.icon id="og-url-copy-icon" name="tabler-copy" class="absolute left-1 my-auto size-4 mr-2" />
-        <.icon
-          id="og-url-check-icon"
-          name="tabler-check"
-          class="absolute left-1 my-auto hidden size-4 mr-2"
+      <%= if @current_org.handle do %>
+        <div class="pt-12 flex items-center justify-between">
+          <h2 class="text-xl font-semibold leading-none tracking-tight">Share your bounty board</h2>
+        </div>
+        <.badge
+          id="og-url"
+          phx-hook="CopyToClipboard"
+          data-value={url(~p"/org/#{@current_org.handle}/home")}
+          data-notice="Copied to clipboard!"
+          phx-click={
+            %JS{}
+            |> JS.hide(
+              to: "#og-url-copy-icon",
+              transition: {"transition-opacity", "opacity-100", "opacity-0"}
+            )
+            |> JS.show(
+              to: "#og-url-check-icon",
+              transition: {"transition-opacity", "opacity-0", "opacity-100"}
+            )
+          }
+          class="relative cursor-pointer mt-3 text-foreground/90 hover:text-foreground"
+          variant="outline"
+        >
+          <.icon id="og-url-copy-icon" name="tabler-copy" class="absolute left-1 my-auto size-4 mr-2" />
+          <.icon
+            id="og-url-check-icon"
+            name="tabler-check"
+            class="absolute left-1 my-auto hidden size-4 mr-2"
+          />
+          <span class="pl-4">{AlgoraWeb.Endpoint.host()}{~p"/org/#{@current_org.handle}/home"}</span>
+        </.badge>
+        <img
+          src={~p"/og/org/#{@current_org.handle}/home"}
+          alt={@current_org.name}
+          class="mt-3 w-full aspect-[1200/630] rounded-lg ring-1 ring-input bg-black"
         />
-        <span class="pl-4">{AlgoraWeb.Endpoint.host()}{~p"/org/#{@current_org.handle}/home"}</span>
-      </.badge>
-      <img
-        src={~p"/og/org/#{@current_org.handle}/home"}
-        alt={@current_org.name}
-        class="mt-3 w-full aspect-[1200/630] rounded-lg ring-1 ring-input bg-black"
-      />
+      <% end %>
       <div class="fixed right-0 bottom-0 hidden w-96 p-4 pr-0 pt-6 sm:p-6 sm:pr-0 md:py-8 md:pr-0 lg:grid lg:grid-cols-2 gap-2 gap-x-4 text-sm">
         <div class="flex items-center gap-2">
           <div class="flex -space-x-2">

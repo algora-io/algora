ec13d7697483e4b10a286044fdf004f2d9e0cafe
zafer <zafer@algora.io>
2025-03-27 11:45:28 +0200

    fix: optimize image loading and styling in HomeLive component
    - Changed image loading attribute to 'lazy' for improved performance.
    - Updated image class for better visual consistency and accessibility.

--- lib/algora_web/live/home_live.ex
+++ lib/algora_web/live/home_live.ex
@@ -848,9 +848,8 @@ defmodule AlgoraWeb.HomeLive do
                   alt="Algora dashboard"
                   width="1200"
                   height="630"
-                  decoding="async"
-                  data-nimg="1"
-                  class="hidden dark:block border border-border bg-white mix-blend-overlay dark:[box-shadow:0px_80px_60px_0px_rgba(0,0,0,0.35),0px_35px_28px_0px_rgba(0,0,0,0.25),0px_18px_15px_0px_rgba(0,0,0,0.20),0px_10px_8px_0px_rgba(0,0,0,0.17),0px_5px_4px_0px_rgba(0,0,0,0.14),0px_2px_2px_0px_rgba(0,0,0,0.10)] [box-shadow:0px_82px_105px_0px_#E3E2DF7A,0px_29.93px_38.33px_0px_#E3E2DF54,0px_14.53px_18.61px_0px_#E3E2DF44,0px_7.12px_9.12px_0px_#E3E2DF36,0px_2.82px_3.61px_0px_#E3E2DF26]"
+                  loading="lazy"
+                  class="border border-border bg-muted mix-blend-overlay dark:[box-shadow:0px_80px_60px_0px_rgba(0,0,0,0.35),0px_35px_28px_0px_rgba(0,0,0,0.25),0px_18px_15px_0px_rgba(0,0,0,0.20),0px_10px_8px_0px_rgba(0,0,0,0.17),0px_5px_4px_0px_rgba(0,0,0,0.14),0px_2px_2px_0px_rgba(0,0,0,0.10)] [box-shadow:0px_82px_105px_0px_#E3E2DF7A,0px_29.93px_38.33px_0px_#E3E2DF54,0px_14.53px_18.61px_0px_#E3E2DF44,0px_7.12px_9.12px_0px_#E3E2DF36,0px_2.82px_3.61px_0px_#E3E2DF26]"
                   style="color:transparent"
                   src={~p"/images/screenshots/org-home.png"}
                 />

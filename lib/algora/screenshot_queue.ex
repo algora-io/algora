787eac19d9c7eb0de8b4888c3f40d16b508b05d6
zafer <zafer@algora.io>
2025-03-28 22:30:29 +0200

    feat: add maximum concurrent tasks limit to manage active image generation tasks

--- lib/algora/screenshot_queue.ex
+++ lib/algora/screenshot_queue.ex
@@ -5,6 +5,7 @@ defmodule Algora.ScreenshotQueue do
   require Logger
 
   @timeout 10_000
+  @max_concurrent_tasks 3
 
   def start_link(_) do
     GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
@@ -16,38 +17,64 @@ defmodule Algora.ScreenshotQueue do
 
   @impl true
   def init(:ok) do
-    {:ok, :no_state_needed}
+    {:ok, %{active_tasks: %{}, queue: :queue.new(), waiting: %{}}}
   end
 
   @impl true
-  def handle_call({:generate_image, url, opts}, _from, state) do
-    result =
-      try do
-        task =
-          Task.async(fn ->
-            try do
-              puppeteer_path = Path.join([:code.priv_dir(:algora), "puppeteer", "puppeteer-img.js"])
+  def handle_call({:generate_image, url, opts}, from, state) do
+    active_count = map_size(state.active_tasks)
 
-              case System.cmd("node", [puppeteer_path] ++ build_opts(url, opts)) do
-                {_, 127} -> {:error, :invalid_exec_path}
-                {cmd_response, _} -> {:ok, cmd_response}
-              end
-            rescue
-              e in ErlangError ->
-                %ErlangError{original: error} = e
+    if active_count < @max_concurrent_tasks do
+      {_task, new_state} = start_task(url, opts, from, state)
+      {:noreply, new_state}
+    else
+      queue = :queue.in({url, opts, from}, state.queue)
+      {:noreply, %{state | queue: queue}}
+    end
+  end
 
-                case error do
-                  :enoent -> {:error, :invalid_exec_path}
-                end
+  @impl true
+  def handle_info({ref, result}, state) when is_reference(ref) do
+    {from, new_active_tasks} = Map.pop(state.active_tasks, ref)
+    if is_tuple(from), do: GenServer.reply(from, result)
+
+    case :queue.out(state.queue) do
+      {{:value, {url, opts, next_from}}, new_queue} ->
+        {_task, new_state} = start_task(url, opts, next_from, %{state | active_tasks: new_active_tasks, queue: new_queue})
+        {:noreply, new_state}
+
+      {:empty, _} ->
+        {:noreply, %{state | active_tasks: new_active_tasks}}
+    end
+  end
+
+  @impl true
+  def handle_info({:DOWN, ref, :process, _pid, _reason}, state) do
+    {_from, new_active_tasks} = Map.pop(state.active_tasks, ref)
+    {:noreply, %{state | active_tasks: new_active_tasks}}
+  end
+
+  defp start_task(url, opts, from, state) do
+    task =
+      Task.async(fn ->
+        try do
+          puppeteer_path = Path.join([:code.priv_dir(:algora), "puppeteer", "puppeteer-img.js"])
+
+          case System.cmd("node", [puppeteer_path] ++ build_opts(url, opts)) do
+            {_, 127} -> {:error, :invalid_exec_path}
+            {cmd_response, _} -> {:ok, cmd_response}
+          end
+        rescue
+          e in ErlangError ->
+            %ErlangError{original: error} = e
+
+            case error do
+              :enoent -> {:error, :invalid_exec_path}
             end
-          end)
+        end
+      end)
 
-        Task.await(task, opts[:timeout] || @timeout)
-      catch
-        :exit, {:timeout, _} -> {:error, :timeout}
-      end
-
-    {:reply, result, state}
+    {task, %{state | active_tasks: Map.put(state.active_tasks, task.ref, from)}}
   end
 
   defp build_opts(url, options) do

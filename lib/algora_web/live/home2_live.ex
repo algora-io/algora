f8cbd941d76ba0347f19beab6f02241b757f1919
zafer <zafer@algora.io>
2025-04-06 20:02:11 +0300

    remove pricing contact us card

--- lib/algora_web/live/home2_live.ex
+++ lib/algora_web/live/home2_live.ex
@@ -449,6 +449,64 @@ defmodule AlgoraWeb.Home2Live do
         </section>
 
         <section class="relative isolate py-16 sm:py-40">
+          <div class="mx-auto px-6 lg:px-8">
+            <div class="relative z-10 pb-4 xl:py-16">
+              <div class="mx-auto max-w-7xl sm:text-center">
+                <div class="mx-auto max-w-3xl space-y-2 lg:max-w-none">
+                  <h2 class="font-display text-2xl font-semibold tracking-tight text-foreground sm:text-6xl text-center mb-2 sm:mb-4">
+                    Simple, transparent pricing
+                  </h2>
+                  <p class="text-center font-medium text-base text-muted-foreground mb-12 max-w-2xl mx-auto">
+                    For individuals, OSS communities, and open/closed source companies
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div class="mx-auto lg:max-w-[95rem] mb-8 mt-8">
+              <div class="flex items-start gap-4">
+                <div class="flex-1">
+                  <h3 class="text-2xl font-semibold text-foreground mb-2">
+                    <div class="flex items-center gap-2">
+                      <.icon name="tabler-wallet" class="h-6 w-6 text-emerald-400" /> Payments
+                    </div>
+                  </h3>
+                  <p class="text-base text-foreground-light">
+                    Fund GitHub issues with USD rewards and pay when work is merged. Set up contracts for ongoing development work. Simple, outcome-based payments.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div class="mx-auto grid grid-cols-1 gap-4 lg:gap-8 lg:max-w-[95rem] lg:grid-cols-2">
+              <%= for plan <- @plans1 do %>
+                <AlgoraWeb.PricingLive.pricing_card1 plan={plan} plans={@plans1} />
+              <% end %>
+            </div>
+
+            <div class="mx-auto lg:max-w-[95rem] mt-16 mb-8">
+              <div class="flex items-start gap-4">
+                <div class="flex-1">
+                  <h3 class="text-2xl font-semibold text-foreground mb-2">
+                    <div class="flex items-center gap-2">
+                      <.icon name="tabler-building-store" class="h-6 w-6 text-purple-400" /> Platform
+                    </div>
+                  </h3>
+                  <p class="text-base text-foreground-light">
+                    Connect with top open source talent, increase project visibility, and hire proven contributors
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="mx-auto grid grid-cols-1 gap-4 lg:max-w-[95rem] xl:gap-0">
+              <%= for plan <- @plans2 do %>
+                <AlgoraWeb.PricingLive.pricing_card2 plan={plan} plans={@plans2} />
+              <% end %>
+            </div>
+          </div>
+        </section>
+
+        <section class="relative isolate py-16 sm:py-40">
           <div class="mx-auto max-w-7xl px-6 lg:px-8">
             <h2 class="mb-8 text-3xl font-bold text-card-foreground text-center">
               <span class="text-muted-foreground">The open source</span>

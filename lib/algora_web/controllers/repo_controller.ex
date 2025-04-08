7ddd2e8607a2788199839b9d169be2a6e422bb43
zafer <zafer@algora.io>
2025-04-08 20:18:26 +0300

    feat: enhance repo controller to ensure repository existence before redirecting

--- lib/algora_web/controllers/repo_controller.ex
+++ lib/algora_web/controllers/repo_controller.ex
@@ -3,6 +3,7 @@ defmodule AlgoraWeb.RepoController do
 
   alias Algora.Accounts.User
   alias Algora.Repo
+  alias Algora.Workspace
 
   def index(conn, %{"repo_owner" => repo_owner, "repo_name" => repo_name}) do
     case Repo.get_by(User, provider: "github", provider_login: repo_owner) do
@@ -10,7 +11,13 @@ defmodule AlgoraWeb.RepoController do
         redirect(conn, to: ~p"/#{handle}")
 
       _ ->
-        redirect(conn, to: ~p"/go/#{repo_owner}/#{repo_name}")
+        case Workspace.ensure_repository(Algora.Admin.token(), repo_owner, repo_name) do
+          {:ok, _repo} ->
+            redirect(conn, to: ~p"/go/#{repo_owner}/#{repo_name}")
+
+          {:error, _reason} ->
+            raise AlgoraWeb.NotFoundError
+        end
     end
   end
 end

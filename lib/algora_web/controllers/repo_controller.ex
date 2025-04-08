95f9674df74aca8acde825a4bc049e852bc6102d
Zafer Cesur <zafer@algora.io>
2025-04-08 18:46:18 +0300

    feat: add controller for `/:repo_owner/:repo_name` (#117)

---
+++ lib/algora_web/controllers/repo_controller.ex
@@ -0,0 +1,16 @@
+defmodule AlgoraWeb.RepoController do
+  use AlgoraWeb, :controller
+
+  alias Algora.Accounts.User
+  alias Algora.Repo
+
+  def index(conn, %{"repo_owner" => repo_owner, "repo_name" => repo_name}) do
+    case Repo.get_by(User, provider: "github", provider_login: repo_owner) do
+      %{handle: handle} when is_binary(handle) ->
+        redirect(conn, to: ~p"/#{handle}")
+
+      _ ->
+        redirect(conn, to: ~p"/go/#{repo_owner}/#{repo_name}")
+    end
+  end
+end

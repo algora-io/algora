ad55be948fb62e43b5a8e037d5919ca30e85fc6f
zafer <zafer@algora.io>
2025-04-08 19:04:46 +0300

    chore: change alert severity

--- lib/algora/organizations/organizations.ex
+++ lib/algora/organizations/organizations.ex
@@ -271,12 +271,12 @@ defmodule Algora.Organizations do
       end)
     else
       {:error, error} ->
-        Algora.Admin.alert("Error initializing preview for #{repo_owner}/#{repo_name}: #{inspect(error)}", :error)
+        Algora.Admin.alert("Error initializing preview for #{repo_owner}/#{repo_name}: #{inspect(error)}", :info)
         {:error, error}
     end
   rescue
     error ->
-      Algora.Admin.alert("Error initializing preview for #{repo_owner}/#{repo_name}: #{inspect(error)}", :error)
+      Algora.Admin.alert("Error initializing preview for #{repo_owner}/#{repo_name}: #{inspect(error)}", :info)
       {:error, error}
   end
 end

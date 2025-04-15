36d6dd1233f533f888ff941f55e5cde3f2100d83
zafer <zafer@algora.io>
2025-04-15 13:09:07 +0300

    revert config

--- config/dev.exs
+++ config/dev.exs
@@ -128,13 +128,13 @@ config :phoenix, :plug_init_mode, :runtime
 config :phoenix_live_view, :debug_heex_annotations, true
 
 # Disable swoosh api client as it is only required for production adapters.
-# config :swoosh, :api_client, false
+config :swoosh, :api_client, false
 
-config :algora, Algora.Mailer,
-  adapter: Swoosh.Adapters.Sendgrid,
-  api_key: System.get_env("SENDGRID_API_KEY")
+# config :algora, Algora.Mailer,
+#   adapter: Swoosh.Adapters.Sendgrid,
+#   api_key: System.get_env("SENDGRID_API_KEY")
 
-config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: Algora.Finch
+# config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: Algora.Finch
 
 # Path to install SaladUI components
 config :salad_ui, components_path: Path.join(File.cwd!(), "lib/algora_web/components/ui")

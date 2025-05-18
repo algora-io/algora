f4e99c348f7a162a3b94f65fc5b88d7d0150430b
zafer <zafer@algora.io>
2025-05-19 02:44:57 +0300

    return proper ecto response

--- lib/algora/workspace/jobs/import_stargazer.ex
+++ lib/algora/workspace/jobs/import_stargazer.ex
@@ -11,18 +11,25 @@ defmodule Algora.Workspace.Jobs.ImportStargazer do
   @impl Oban.Worker
   def perform(%Oban.Job{args: %{"provider_logins" => provider_logins, "repo_id" => repo_id}}) do
     with {:ok, users} <- Algora.Workspace.fetch_top_contributions_async(Github.TokenPool.get_token(), provider_logins) do
-      Repo.insert_all(
-        Stargazer,
-        Enum.map(users, fn user ->
-          %{
-            id: Nanoid.generate(),
-            inserted_at: DateTime.utc_now(),
-            updated_at: DateTime.utc_now(),
-            user_id: user.id,
-            repository_id: repo_id
-          }
-        end)
-      )
+      {count, _} =
+        Repo.insert_all(
+          Stargazer,
+          Enum.map(users, fn user ->
+            %{
+              id: Nanoid.generate(),
+              inserted_at: DateTime.utc_now(),
+              updated_at: DateTime.utc_now(),
+              user_id: user.id,
+              repository_id: repo_id
+            }
+          end)
+        )
+
+      if count > 0 do
+        :ok
+      else
+        {:error, :insert_all_failed}
+      end
     end
   end
 

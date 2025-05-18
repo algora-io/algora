ee3d724a6a5367c2724e0aabd87e4e5713efb409
zafer <zafer@algora.io>
2025-05-19 01:19:59 +0300

    feat: show spinner while user is onboarded in bg

--- lib/algora_web/live/onboarding/dev.ex
+++ lib/algora_web/live/onboarding/dev.ex
@@ -91,6 +91,35 @@ defmodule AlgoraWeb.Onboarding.DevLive do
   end
 
   @impl true
+  def render(%{current_user: current_user} = assigns) when not is_nil(current_user) do
+    ~H"""
+    <div
+      class="w-screen h-screen fixed inset-0 bg-background z-[100]"
+      phx-hook="LocalStateStore"
+      id="onboarding-dev-page"
+    >
+      <div class="flex items-center justify-center h-full">
+        <svg
+          class="mr-3 -ml-1 size-12 animate-spin text-success"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4">
+          </circle>
+          <path
+            class="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          >
+          </path>
+        </svg>
+      </div>
+    </div>
+    """
+  end
+
+  @impl true
   def render(assigns) do
     ~H"""
     <div class="min-h-screen bg-card" phx-hook="LocalStateStore" id="onboarding-dev-page">

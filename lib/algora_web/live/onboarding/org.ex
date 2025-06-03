f4ee253e2c0d773d75bf0d5be677e7c48779a1d6
zafer <zafer@algora.io>
2025-06-03 04:48:48 +0300

    make onboarding page responsive

--- lib/algora_web/live/onboarding/org.ex
+++ lib/algora_web/live/onboarding/org.ex
@@ -53,37 +53,51 @@ defmodule AlgoraWeb.Onboarding.OrgLive do
   @impl true
   def render(assigns) do
     ~H"""
-    <main class="bg-black relative overflow-hidden min-h-screen flex flex-col">
+    <main class="bg-background relative overflow-hidden min-h-screen flex flex-col">
       <header class="w-full border-b border-white/20">
-        <div class="flex items-center gap-8 bg-background">
-          <div>
-            <.wordmark class="h-8 w-auto ml-24" />
+        <div class="flex items-center bg-background">
+          <div class="px-6">
+            <.wordmark class="h-8 w-auto" />
           </div>
-          <div class="h-full flex flex-col bg-black border-l border-white/20 pl-8 py-8 -mt-5">
+          <div class="flex-1 h-full flex flex-col bg-black border-l border-white/20 px-6 pl-8 pt-6 pb-3 -mt-5">
             <span class="text-xs text-muted-foreground font-medium">Trusted by</span>
-            <div class="mt-1 flex items-center justify-center gap-12">
-              <img src="/images/wordmarks/keep.png" alt="Keep" class="h-10 saturate-0" />
-              <img src="/images/wordmarks/triggerdotdev.png" alt="Trigger.dev" class="h-6 saturate-0" />
-              <img src="/images/wordmarks/traceloop.png" alt="Traceloop" class="h-6 saturate-0" />
-              <img src="/images/wordmarks/million.png" alt="Million" class="h-6 saturate-0" />
-              <img src="/images/wordmarks/moonrepo.svg" alt="moon" class="h-5" />
-              <img
-                src="/images/wordmarks/dittofeed.png"
-                alt="Dittofeed"
-                class="h-6 brightness-0 invert"
-              />
-              <img
-                src={~p"/images/wordmarks/highlight.png"}
-                alt="Highlight"
-                class="h-6 saturate-0"
-                loading="lazy"
-              />
+            <div class="relative w-full">
+              <div class="mt-1 flex items-center gap-12 overflow-x-auto scrollbar-thin pb-4">
+                <img src="/images/wordmarks/keep.png" alt="Keep" class="h-10 saturate-0 shrink-0" />
+                <img
+                  src="/images/wordmarks/triggerdotdev.png"
+                  alt="Trigger.dev"
+                  class="h-6 saturate-0 shrink-0"
+                />
+                <img
+                  src="/images/wordmarks/traceloop.png"
+                  alt="Traceloop"
+                  class="h-6 saturate-0 shrink-0"
+                />
+                <img
+                  src="/images/wordmarks/million.png"
+                  alt="Million"
+                  class="h-6 saturate-0 shrink-0"
+                />
+                <img src="/images/wordmarks/moonrepo.svg" alt="moon" class="h-5 shrink-0" />
+                <img
+                  src="/images/wordmarks/dittofeed.png"
+                  alt="Dittofeed"
+                  class="h-6 brightness-0 invert shrink-0"
+                />
+                <img
+                  src={~p"/images/wordmarks/highlight.png"}
+                  alt="Highlight"
+                  class="h-6 saturate-0 shrink-0"
+                  loading="lazy"
+                />
+              </div>
             </div>
           </div>
         </div>
       </header>
 
-      <div class="flex-1 py-4 flex items-center justify-center max-h-[calc(100vh-11rem)] overflow-y-auto scrollbar-thin">
+      <div class="flex-1 p-4 md:py-4 flex items-center justify-center max-h-[calc(100vh-11rem)] overflow-y-auto scrollbar-thin">
         <div class="w-full max-w-[28rem] text-left">
           <.form for={@form} phx-submit="submit" class="flex flex-col gap-6">
             <.input
@@ -97,6 +111,7 @@ defmodule AlgoraWeb.Onboarding.OrgLive do
               type="textarea"
               label="Job description / careers URL"
               rows="3"
+              class="resize-none"
               placeholder="Tell us about the role and your requirements..."
             />
             <.input
@@ -104,6 +119,7 @@ defmodule AlgoraWeb.Onboarding.OrgLive do
               type="textarea"
               label="Describe your ideal candidate, heuristics, green/red flags etc."
               rows="3"
+              class="resize-none"
               placeholder={placeholder_text()}
             />
             <div class="flex flex-col gap-4">
@@ -119,12 +135,12 @@ defmodule AlgoraWeb.Onboarding.OrgLive do
       <footer class="w-full py-4 border-t border-white/20">
         <div class="container mx-auto px-4">
           <div class="flex flex-col md:flex-row items-center justify-between gap-6">
-            <div class="text-sm text-muted-foreground">
+            <div class="text-sm text-muted-foreground text-center md:text-left w-full md:w-auto">
               © 2025 Algora PBC. All rights reserved.
             </div>
-            <div class="flex items-center gap-2">
+            <div class="grid grid-cols-1 md:flex md:flex-row items-stretch gap-2 w-full md:w-auto">
               <.link
-                class="flex w-max items-center gap-2 rounded-lg border border-gray-700 py-2 pl-2 pr-3.5 text-xs text-muted-foreground hover:text-foreground transition-colors hover:border-gray-600"
+                class="w-full md:w-auto flex items-center justify-center gap-2 rounded-lg border border-gray-700 py-2 pl-2 pr-3.5 text-xs text-muted-foreground hover:text-foreground transition-colors hover:border-gray-600"
                 href={AlgoraWeb.Constants.get(:calendar_url)}
                 rel="noopener"
               >
@@ -132,13 +148,13 @@ defmodule AlgoraWeb.Onboarding.OrgLive do
                 <span>Schedule a call</span>
               </.link>
               <.link
-                class="flex w-max items-center gap-2 rounded-lg border border-gray-700 py-2 pl-2 pr-3.5 text-xs text-muted-foreground hover:text-foreground transition-colors hover:border-gray-600"
+                class="w-full md:w-auto flex items-center justify-center gap-2 rounded-lg border border-gray-700 py-2 pl-2 pr-3.5 text-xs text-muted-foreground hover:text-foreground transition-colors hover:border-gray-600"
                 href="tel:+16504202207"
               >
                 <.icon name="tabler-phone" class="size-4" /> US <span>+1 (650) 420-2207</span>
               </.link>
               <.link
-                class="flex w-max items-center gap-2 rounded-lg border border-gray-700 py-2 pl-2 pr-3.5 text-xs text-muted-foreground hover:text-foreground transition-colors hover:border-gray-600"
+                class="w-full md:w-auto flex items-center justify-center gap-2 rounded-lg border border-gray-700 py-2 pl-2 pr-3.5 text-xs text-muted-foreground hover:text-foreground transition-colors hover:border-gray-600"
                 href="tel:+306973184144"
               >
                 <.icon name="tabler-phone" class="size-4" /> EU <span>+30 (697) 318-4144</span>

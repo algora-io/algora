3343a3ab59d91a952e8abe640fd4244a1cc1efee
zafer <zafer@algora.io>
2025-08-29 17:47:36 +0300

    add :rest attr in multiline components

--- lib/algora_web/components/ui/multiline.ex
+++ lib/algora_web/components/ui/multiline.ex
@@ -4,19 +4,21 @@ defmodule AlgoraWeb.Components.UI.Multiline do
 
   attr :value, :string, required: true
   attr :class, :string, default: nil
+  attr :rest, :global
 
   def multiline(assigns) do
     ~H"""
-    <div class={classes(["whitespace-pre-line", @class])}>{@value}</div>
+    <div class={classes(["whitespace-pre-line", @class])} {@rest}>{@value}</div>
     """
   end
 
   attr :value, :string, required: true
   attr :class, :string, default: nil
+  attr :rest, :global
 
   def markdown(assigns) do
     ~H"""
-    <div class={classes(["whitespace-normal", @class])}>{Phoenix.HTML.raw(@value)}</div>
+    <div class={classes(["whitespace-normal", @class])} {@rest}>{Phoenix.HTML.raw(@value)}</div>
     """
   end
 end

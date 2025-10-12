e9a0508a1e2884e11143270379405539b164a629
zafer <zafer@algora.io>
2025-10-12 13:00:12 +0300

    add slugify helper

--- lib/algora/shared/util.ex
+++ lib/algora/shared/util.ex
@@ -292,4 +292,15 @@ defmodule Algora.Util do
     |> DateTime.shift_zone!("Europe/Athens", Tzdata.TimeZoneDatabase)
     |> Calendar.strftime("%Y-%m-%d %H:%M:%S")
   end
+
+  def slugify(text) when is_binary(text) do
+    text
+    |> String.downcase()
+    |> String.trim()
+    |> String.normalize(:nfd)
+    |> String.replace(~r/[^a-z0-9\s-]/u, "  ")
+    |> String.replace(~r/[\s-]+/, "-", global: true)
+  end
+
+  def slugify(_), do: ""
 end

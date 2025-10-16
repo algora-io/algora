910db598136c31003e635b3b1b1871de801e2051
zafer <zafer@algora.io>
2025-10-16 16:28:18 +0300

    update s3 bucket url

--- lib/algora/s3.ex
+++ lib/algora/s3.ex
@@ -1,14 +1,11 @@
 defmodule Algora.S3 do
   @moduledoc false
 
-  def endpoint_url do
-    config = Application.fetch_env!(:ex_aws, :s3)
-    "#{config[:scheme]}#{config[:host]}"
-  end
-
   def bucket_name, do: Algora.config([:bucket_name])
 
-  def bucket_url, do: endpoint_url() <> "/" <> bucket_name()
+  def bucket_url, do: "#{AlgoraWeb.Endpoint.url()}/storage"
+
+  def bucket_url(path), do: Path.join(bucket_url(), path)
 
   def upload(body, object, opts \\ []) do
     bucket_name()

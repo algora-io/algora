2c30a93307ce446f06d93aab1ab6b92019516424
zafer <zafer@algora.io>
2026-02-11 15:45:13 +0200

    update migration to create index concurrently

--- priv/repo/migrations/20260211152553_add_contract_signed_index_to_users.exs
+++ priv/repo/migrations/20260211152553_add_contract_signed_index_to_users.exs
@@ -1,7 +1,10 @@
 defmodule Algora.Repo.Migrations.AddContractSignedIndexToUsers do
   use Ecto.Migration
 
+  @disable_ddl_transaction true
+  @disable_migration_lock true
+
   def change do
-    create index(:users, [:contract_signed])
+    create index(:users, [:contract_signed], concurrently: true)
   end
 end

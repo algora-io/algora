d4c37877dd7c8f86c1d6d9e3da1d98f59d8c4ebc
zafer <zafer@algora.io>
2026-03-23 21:29:02 +0300

    restyle disabled buttons

--- lib/algora_web/components/ui/button.ex
+++ lib/algora_web/components/ui/button.ex
@@ -33,7 +33,7 @@ defmodule AlgoraWeb.Components.UI.Button do
       |> assign(:is_link, link?(assigns))
       |> assign(:common_classes, [
         "disabled:opacity-75 phx-submit-loading:opacity-75",
-        "inline-flex items-center justify-center whitespace-nowrap rounded-lg text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+        "inline-flex items-center justify-center whitespace-nowrap rounded-lg text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:opacity-50 disabled:cursor-not-allowed",
         variant(assigns),
         assigns.class
       ])
